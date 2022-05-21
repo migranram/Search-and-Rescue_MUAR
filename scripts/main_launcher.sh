dc5b6834aa5d4caff255b767e88abf1498910c73
Miguel G <miguelgranero99@gmail.com>
2022-05-21 20:29:02 +0200

    launcher script chnaged

--- scripts/main_launcher.sh
+++ scripts/main_launcher.sh
@@ -6,18 +6,15 @@ tmux -2 new-session -d -s $session
 # Create roscore 
 # send-keys writes the string into the sesssion (-t -> target session , C-m -> press Enter Button)
 tmux new-window -t $session:0 -n 'gazebo'
-tmux send-keys -t $session:0 "roslaunch sar_simulations no_gui.launch \
-    drone_id:=$drone_namespace " C-m
+tmux send-keys -t $session:0 "roslaunch sar_simulations no_gui.launch" C-m
 
 sleep 5
 
 tmux new-window -t $session:1 -n 'slam'
-tmux send-keys -t $session:1 "roslaunch sar_SLAM hector_slam.launch \
-    drone_id:=$drone_namespace " C-m
+tmux send-keys -t $session:1 "roslaunch sar_SLAM hector_slam.launch" C-m
 
 tmux new-window -t $session:2 -n 'move_base'
-tmux send-keys -t $session:2 "roslaunch sar_SLAM move_base.launch  \
-    drone_id:=$drone_namespace " C-m
+tmux send-keys -t $session:2 "roslaunch sar_SLAM move_base.launch" C-m
 
 echo Finished!
 
